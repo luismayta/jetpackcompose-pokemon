a5cb80074dc322a1c0407866fd2334672b1d8d8e
Luis Mayta <luis@hadenlabs.com>
2024-09-29 17:37:06 -0300

    chore(core): implement const (#5)

--- app/src/main/java/com/luismayta/jetpackcomposepokemon/core/Constants.kt
+++ app/src/main/java/com/luismayta/jetpackcomposepokemon/core/Constants.kt
@@ -1,7 +1,11 @@
 package com.luismayta.jetpackcomposepokemon.core
 
-object Constants {
-    const val IMAGES_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/"
-    const val BASE_URL = "https://pokeapi.co/api/v2/"
-    const val POKEMON_DATABASE = "pokemon_database"
+class Constants {
+
+  object Endpoints {
+    const val ENDPOINT = "https://pokeapi.co/api/v2/"
+    const val IMAGES_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/"
+    const val CARDS = "cards"
+    const val PAGE_SIZE = "pageSize"
+  }
 }
