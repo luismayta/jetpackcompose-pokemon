d1648d64dc5f839e205820fe90a0730c20592946
Luis Mayta <luis@hadenlabs.com>
2024-10-20 23:50:57 -0300

    feat(screen): separated pokemon list (#24)

--- app/src/main/java/com/luismayta/jetpackcomposepokemon/presentation/screen/pokemonlistscreen/components/PokemonCell.kt
+++ app/src/main/java/com/luismayta/jetpackcomposepokemon/presentation/screen/pokemonlistscreen/components/PokemonCell.kt
@@ -11,39 +11,50 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.luismayta.jetpackcomposepokemon.domain.model.PokemonModel
 
-
 @Composable
 fun PokemonCell(pokemon: PokemonModel) {
+  Row(
+    verticalAlignment = Alignment.CenterVertically,
+    modifier = Modifier.padding(8.dp)
+  ) {
+    AsyncImage(
+      model = pokemon.imageUrl,
+      "Pokémon image",
+      modifier = Modifier
+          .size(60.dp)
+          .clip(CircleShape)
+          .background(Color.Gray.copy(alpha = 0.1f))
+          .padding(8.dp)
+    )
+    Text(
+      "#${pokemon.id}",
+      style = MaterialTheme.typography.bodySmall,
+      fontWeight = FontWeight.Light,
+      modifier = Modifier.padding(4.dp)
+    )
+    Text(
+      pokemon.name.replaceFirstChar { it.uppercase() },
+      style = MaterialTheme.typography.bodySmall,
+      fontWeight = FontWeight.Medium
+    )
+    Spacer(modifier = Modifier.fillMaxWidth())
+  }
 
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier.padding(8.dp)
-    ) {
-        AsyncImage(
-            model = pokemon.imageUrl,
-            "Pokémon image",
-            modifier = Modifier
-                .size(60.dp)
-                .clip(CircleShape)
-                .background(Color.Gray.copy(alpha = 0.1f))
-                .padding(8.dp)
-        )
-        Text(
-            "#${pokemon.id}",
-            style = MaterialTheme.typography.bodySmall,
-            fontWeight = FontWeight.Light,
-            modifier = Modifier.padding(4.dp)
-        )
-        Text(
-            pokemon.name.replaceFirstChar { it.uppercase() },
-            style = MaterialTheme.typography.bodySmall,
-            fontWeight = FontWeight.Medium
-        )
-        Spacer(modifier = Modifier.fillMaxWidth())
-    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun PokemonCellPreview() {
+  val dataPokemonModel =
+    PokemonModel(name = "bulbasaur", url = "https://pokeapi.co/api/v2/pokemon/1/")
+
+  return PokemonCell(
+    pokemon = dataPokemonModel
+  )
 
 }
